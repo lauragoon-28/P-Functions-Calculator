af87d8c40e7a9be0114697c11ebe15e10a9192e9
Laura Goon <Laura.goon@ou.edu>
2021-04-01 18:16:20 -0500

    started this participation

---
+++ P-Functions_Calculator/P-Functions_Calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace P_Functions_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static double Add(double val1, double val2) 
+        {
+            double sum = val1 + val2;
+
+            return sum;
+
+            //return val1 + val2;
+        }
+
+        static double Subtract(double val1, double val2)
+        {
+            double difference = val1 - val2;
+
+            return difference;
+        }
+
+        static double Multiply(double val1, double val2)
+        {
+            double product = val1 * val2;
+
+            return product;
+        }
+
+        static double Divide(double val1, double val2)
+        {
+            double quotient = val1 / val2;
+
+            return quotient;
+        }
+
+        static string developerInformation(string name, string className, string date)
+        {
+            return 0;
+        }
+    }
+}
